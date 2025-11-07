b320aae1f4b8356edde8a3bed290c0ca76424993
Venkatesh2304 <96430022+Venkatesh2304@users.noreply.github.com>
2025-11-07 20:44:31 +0530

    s

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 PROJECT_NAME="myerpv2" # For the service name
-PORT=3000
+PORT=4000
 
 git pull -ff
 FRONTEND_DIR="$(cd "$(dirname "$0")" && pwd)"
-pm2 start "serve -s $FRONTEND_DIR -l $PORT" --name $PROJECT_NAME-frontend
+pm2 delete $PROJECT_NAME-frontend || true
+pm2 start "serve -s $FRONTEND_DIR/dist -l $PORT" --name $PROJECT_NAME-frontend
 pm2 save
