1320a2398a82a5ef2c65a05c798fe44bf2923b97
Venkatesh2304 <96430022+Venkatesh2304@users.noreply.github.com>
2025-11-07 21:12:15 +0530

    s

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  output: "export",
-  distDir: "dist",  // optional: name of output directory
 };
 
 export default nextConfig;
--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
 git pull -ff
 pm2 restart myerpv2-frontend
