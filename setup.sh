d312cf105670d8db8659b0c32797c99ae9976b67
Venkatesh2304 <96430022+Venkatesh2304@users.noreply.github.com>
2025-11-07 20:26:55 +0530

    s

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ PROJECT_NAME="myerpv2" # For the service name
 PORT=3000
 
 git pull -ff
-FRONTEND_DIR="$(cd "$(dirname "$0")" && pwd")"
+FRONTEND_DIR="$(cd "$(dirname "$0")" && pwd)"
 pm2 start "serve -s $FRONTEND_DIR -l $PORT" --name $PROJECT_NAME-frontend
 pm2 save
